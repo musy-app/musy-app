487a464a83a2ab4387ad539f581856713c674ded
Jérémy <work@jeremybdn.fr>
2023-05-10 17:49:56 +0200

    💚 update size-limit config

--- .size-limit.js
+++ .size-limit.js
@@ -23,11 +23,35 @@ module.exports = [
     modifyWebpackConfig,
   },
   {
-    path: "packages/shared-types/next/dist/musy-app-shared-types.cjs.prod.js",
+    path: "packages/shared-types/dist/musy-app-shared-types.cjs.prod.js",
     modifyWebpackConfig,
   },
   {
-    path: "packages/shared-types/next/dist/musy-app-shared-types.esm.js",
+    path: "packages/shared-types/dist/musy-app-shared-types.esm.js",
+    modifyWebpackConfig,
+  },
+  {
+    path: "packages/kitchen/dist/musy-app-groovy.cjs.prod.js",
+    modifyWebpackConfig,
+  },
+  {
+    path: "packages/kitchen/dist/musy-app-groovy.esm.js",
+    modifyWebpackConfig,
+  },
+  {
+    path: "packages/kitchen/next/dist/musy-app-groovy-next.cjs.prod.js",
+    modifyWebpackConfig,
+  },
+  {
+    path: "packages/kitchen/next/dist/musy-app-groovy-next.esm.js",
+    modifyWebpackConfig,
+  },
+  {
+    path: "packages/kitchen/native/dist/musy-app-groovy-native.cjs.prod.js",
+    modifyWebpackConfig,
+  },
+  {
+    path: "packages/kitchen/native/dist/musy-app-groovy-native.esm.js",
     modifyWebpackConfig,
   },
 ];
